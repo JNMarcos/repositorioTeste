099d6434ca565949ea5327b1da28de5b5d23b902
João Nascimento <jn.marcos@outlook.com>
2025-10-07 22:06:09 -0300

    Adição da segunda classe com string proibida

---
+++ src/DocumentMINHACLASSE2.java
@@ -0,0 +1,22 @@
+
+public class DocumentMINHACLASSE2 {
+	
+	private String id;
+	private Integer number;
+	// 'um dois três'
+	// cada um no seu quadrado
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+}
